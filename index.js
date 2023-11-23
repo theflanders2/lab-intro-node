1570291a666f136eff999a918822f40865cd089c
theflanders2 <kenneth.flanders.ii@gmail.com>
2023-11-23 09:43:46 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,86 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // console.log(`array before: ${this.items}`)
+    this.items.push(item);
+    this.items.sort(function(a, b){return a - b});
+    // console.log(`array after: ${this.items}`)
+    // console.log(`array length before: ${this.length}`)
+    this.length++;
+    // console.log(`array length after: ${this.length}`)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length - 1){
+      // console.log(`value at position ${pos}: ${this.items[pos]}`);
+      return this.items[pos];
+    }
+    else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    let max = 0;
 
-  min() {}
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      for (let i = 0; i < this.length; i++) {
+        if (this.items[i] > max) {
+          max = this.items[i];
+          // console.log(`current max: ${max}`)
+        }
+      }
+      return max;
+    }
+  }
 
-  sum() {}
+  min() {
+    let min = 100;
 
-  avg() {}
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      for (let i = 0; i < this.length; i++) {
+        if (this.items[i] <= min) {
+          min = this.items[i];
+          // console.log(`current min: ${min}`)
+        }
+      }
+      return min;
+    }
+  }
+
+  sum() {
+    let sum = 0;
+
+    if (this.length === 0) {
+      return 0;
+    }
+    else {
+      for (let i = 0; i < this.length; i++) {
+        sum += this.items[i];
+      }
+      // console.log(`sum: ${sum}`)
+      return sum;
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      }
+      else {
+        return this.sum() / this.length;
+      }
+  }
 }
 
 module.exports = SortedList;
